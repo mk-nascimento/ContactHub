0904f452bcb1606c72a9a2432848b9880e6756bf
Maksuel Nascimento <mkdias_nascimento@hotmail.com>
2023-08-01 18:53:25 -0300

    feat: remove deleted file import

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import './App.css';
 import App from './App.tsx';
 import { AuthProvider } from './Providers/Auth.provider.tsx';
 import { ContactsProvider } from './Providers/ContactProvider.tsx';
