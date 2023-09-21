a4e3ae1638f885c44708fba5749a25f0a7e34ba0
Maksuel Nascimento <mkdias_nascimento@hotmail.com>
2023-09-21 20:12:44 -0300

    fix: correct image logo imports
    - Fixed image logo imports to ensure proper file paths

--- frontend/src/components/Container/Disconnected/index.tsx
+++ frontend/src/components/Container/Disconnected/index.tsx
@@ -1,7 +1,7 @@
 import { useLocation } from 'react-router-dom';
+import greenLogo from '../../../assets/green.lo-go.svg';
+import whiteLogo from '../../../assets/white.lo-go.svg';
 import { Pathnames } from '../../../enums/paths';
-import greenLogo from '../../assets/green.lo-go.svg';
-import whiteLogo from '../../assets/white.lo-go.svg';
 
 interface IDisconnectedContainerProps {
   children: React.ReactNode;
@@ -13,20 +13,20 @@ export const DisconnectedContainer = (props: IDisconnectedContainerProps) => {
   const isNotDashboard: boolean = pathname !== Pathnames.Dashboard;
 
   return (
-    <div className="min-h-screen md:flex md:flex-row-reverse md:bg-[url('/src/assets/phone.svg')] md:bg-cover md:bg-opacity-95 md:bg-no-repeat">
-      <aside className="relative flex justify-center bg-[url('/src/assets/phone.svg')] bg-opacity-95 bg-cover bg-no-repeat w-full h-64 md:w-1/2 md:h-full md:bg-none">
+    <div className="min-h-screen md:flex md:flex-row-reverse md:bg-opacity-95 md:bg-[url('/src/assets/phone.svg')] md:bg-cover md:bg-no-repeat">
+      <aside className="relative flex h-64 w-full justify-center bg-opacity-95 bg-[url('/src/assets/phone.svg')] bg-cover bg-no-repeat md:h-full md:w-1/2 md:bg-none">
         {isNotDashboard ? <img src={whiteLogo} alt='Logo' className='w-[210px] md:hidden' /> : null}
 
-        <div className='absolute -bottom-0 w-full h-[69px] rounded-t-full bg-brand-100 md:hidden'>
-          <div className='absolute -bottom-0 w-full h-[59px] rounded-t-full bg-brand-200'>
-            <div className='absolute -bottom-0 w-full h-[49px] rounded-t-full bg-grey-50'></div>
+        <div className='absolute -bottom-0 h-[69px] w-full rounded-t-full bg-brand-100 md:hidden'>
+          <div className='absolute -bottom-0 h-[59px] w-full rounded-t-full bg-brand-200'>
+            <div className='absolute -bottom-0 h-[49px] w-full rounded-t-full bg-grey-50'></div>
           </div>
         </div>
       </aside>
 
-      <main className='bg-grey-50 grid content-center justify-items-center grid-cols-1 gap-[30px] px-12 pb-12 w-full md:flex-1 md:py-16 md:max-h-full'>
+      <main className='grid w-full grid-cols-1 content-center justify-items-center gap-[30px] bg-grey-50 px-12 pb-12 md:max-h-full md:flex-1 md:py-16'>
         {isNotDashboard ? (
-          <header className='hidden md:block w-[210px]'>
+          <header className='hidden w-[210px] md:block'>
             <img src={greenLogo} alt='Logo' />
           </header>
         ) : null}
