f8c5cda0985cf4756eb303fd0caa87d44f6849be
Maksuel Nascimento <mkdias_nascimento@hotmail.com>
2023-09-21 17:52:41 -0300

    feat: refactor UserProvider
    - Refactored UserProvider to utilize useRequest for handling requests
    - Updated typings and context return values for improved consistency

--- frontend/src/providers/User.provider.tsx
+++ frontend/src/providers/User.provider.tsx
@@ -1,28 +1,11 @@
-import { AxiosError } from 'axios';
-import pdfMake from 'pdfmake/build/pdfmake';
-import pdfFonts from 'pdfmake/build/vfs_fonts';
-import { createContext, useEffect, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
-
-import { TDocumentDefinitions } from 'pdfmake/interfaces';
-import { Contact } from '../interfaces/global.interfaces';
+import Cookies from 'js-cookie';
+import { createContext, useCallback, useMemo, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Pathnames } from '../enums/paths';
+import { useRequest } from '../hooks/useRequest';
+import { Contact } from '../interfaces';
 import { TUserData } from '../schemas';
 import api from '../services/axios';
-
-interface UserContextValues {
-  getProfile: () => Promise<void>;
-  registerUser: (data: TUserData) => Promise<void>;
-  updateUser: (data: TUserData) => void;
-  deleteUser: () => Promise<void>;
-  isOpenModal: boolean;
-  setIsOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
-  profile: UserProfile | undefined;
-  exportProfile: (data: UserProfile) => void;
-  deleteProfileModal: boolean;
-  setDeleteProfileModal: React.Dispatch<React.SetStateAction<boolean>>;
-}
 
 interface UserProviderProps {
   children: React.ReactNode;
@@ -40,121 +23,68 @@ interface UserProfile extends UserResponse {
   contacts: Contact[];
 }
 
+interface UserService {
+  profile: () => Promise<void>;
+  register: (data: TUserData) => Promise<void>;
+  update: (updateData: TUserData) => Promise<void>;
+  destroy: () => Promise<void>;
+}
+
+interface UserContextValues {
+  userService: UserService;
+}
+
 export const UserContext = createContext({} as UserContextValues);
 
 export const UserProvider = ({ children }: UserProviderProps) => {
   const navigate = useNavigate();
-  const { pathname }: Partial<Location> = useLocation();
-  const [deleteProfileModal, setDeleteProfileModal] = useState<boolean>(false);
-  const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
-  const [profile, setProfile] = useState<UserProfile>();
-  const token: string | null = localStorage.getItem('@fullstack-challenge:token');
+  const [userProfile, setUserProfile] = useState<UserProfile>();
 
-  useEffect(() => {
-    if (token) api.defaults.headers.common.Authorization = `Bearer ${token}`;
+  const { data: profileData, request: profileRequest } = useRequest<UserProfile>();
+  const { data: registerData, request: registerRequest } = useRequest<UserResponse>();
+  const { request: updateRequest } = useRequest<UserResponse>();
+  const { data: loginData, request: loginRequest } = useRequest<{ token: string }>();
+  const { request: destroyRequest } = useRequest();
 
-    const validate = async () => {
-      try {
-        await api.get('auth/validate/');
-      } catch (error) {
-        if (error instanceof AxiosError && error.response?.status === 401 && (pathname === '/dashboard' || pathname === '/profile')) navigate('/');
+  const profile = useCallback(async () => {
+    await profileRequest(() => api.get('profile'));
+
+    if (profileData) setUserProfile(profileData);
+  }, [profileData, profileRequest]);
+
+  const register = useCallback(
+    async (data: TUserData) => {
+      await registerRequest(() => api.post('users', data));
+
+      if (registerData) {
+        await loginRequest(() => api.post('auth/login', data));
+
+        if (loginData?.token) {
+          Cookies.set('token', loginData.token, { secure: true });
+          navigate(Pathnames.Homepage);
+        }
       }
-    };
-    if (api.defaults.headers.common.Authorization) validate();
+    },
+    [loginData, loginRequest, navigate, registerData, registerRequest],
+  );
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [token]);
+  const update = useCallback(
+    async (updateData: TUserData) => {
+      await updateRequest(() => api.patch(`users/${userProfile?.id}`, updateData));
+    },
+    [userProfile, updateRequest],
+  );
 
-  const getProfile = async () => {
-    try {
-      const { data, status } = await api.get<UserProfile>('profile/');
-      if (status === 200) setProfile((prev) => (prev !== data ? data : profile));
-    } catch (error) {
-      if (error instanceof AxiosError) navigate('/dashboard');
-    }
-  };
+  const destroy = useCallback(async () => {
+    await destroyRequest(() => api.delete(`users/${userProfile?.id}`));
+  }, [userProfile, destroyRequest]);
 
-  const registerUser = async (data: TUserData) => {
-    try {
-      const response = await api.post<UserResponse>('users/', data);
-      const { id } = response.data;
-      if (id) {
-        const loginResponse = await api.post<{ token: string }>('auth/login/', data);
+  const userService: UserService = useMemo(
+    () => ({ register, profile, update, destroy }),
+    [register, profile, update, destroy],
+  );
 
-        const { token } = loginResponse.data;
+  const values = useMemo(() => ({ userService }), [userService]);
 
-        api.defaults.headers.common.Authorization = `Bearer ${token}`;
-        localStorage.setItem('@fullstack-challenge:token', token);
-
-        if (token) navigate('dashboard');
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const updateUser = async (updateData: TUserData) => {
-    try {
-      const { status } = await api.patch<UserResponse>(`users/${profile?.id}`, updateData);
-      if (status === 200) setIsOpenModal(false);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const deleteUser = async () => {
-    try {
-      await api.delete(`users/${profile?.id}`);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const exportProfile = (data: UserProfile) => {
-    const def: TDocumentDefinitions = {
-      content: [
-        { text: 'Informações de usuário', style: 'header' },
-        { text: `ID: ${data.id}` },
-        { text: `Name: ${data.full_name}` },
-        { text: `Email: ${data.email}` },
-        { text: `Created At: ${data.created_at}` },
-        { text: 'Contatos', style: 'subheader' },
-        data.contacts?.map((contact) => [
-          { text: `ID: ${contact.id}` },
-          { text: `Full Name: ${contact.full_name}` },
-          { text: `Email: ${contact.email}` },
-          { text: `Cellphone: ${contact.cellphone}` },
-          { text: `Created At: ${contact.created_at}` },
-          { text: '' },
-        ]),
-      ],
-      styles: {
-        header: {
-          fontSize: 20,
-          bold: true,
-          margin: [0, 0, 0, 10],
-        },
-        subheader: {
-          fontSize: 18,
-          bold: true,
-          margin: [0, 10, 0, 5],
-        },
-      },
-    };
-    pdfMake.createPdf(def).download('export_profile.pdf');
-  };
-
-  const values = {
-    getProfile,
-    registerUser,
-    updateUser,
-    deleteUser,
-    isOpenModal,
-    setIsOpenModal,
-    profile,
-    exportProfile,
-    deleteProfileModal,
-    setDeleteProfileModal,
-  };
   return <UserContext.Provider value={values}>{children}</UserContext.Provider>;
 };
