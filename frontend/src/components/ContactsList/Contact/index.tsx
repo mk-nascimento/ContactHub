c021e48bc2db7272e426c4d61de9d158a99c9c85
Maksuel Nascimento <114680477+mk-nascimento@users.noreply.github.com>
2023-10-22 16:32:03 -0300

    feat: Add optional 'view' prop for contact list component for view-only behavior
    - Implemented behavior controlled by 'view' prop to enable view-only mode in the contact list component

--- frontend/src/components/ContactsList/Contact/index.tsx
+++ frontend/src/components/ContactsList/Contact/index.tsx
@@ -4,19 +4,20 @@ import { phoneReplaceRegex } from 'utils/string.utils';
 
 interface Props extends LiHTMLAttributes<HTMLLIElement> {
   contact: Contact;
+  view?: boolean;
 }
 
-export const ContactCard = ({ contact, ...rest }: Props) => (
+export const ContactCard = ({ contact, view, ...rest }: Props) => (
   <li
     {...rest}
-    className='text-14-400 grid h-8 grid-cols-2 items-center gap-x-[8px] rounded-[6px] px-[16px] capitalize first:pl-[16px] last:pr-[16px] hover:scale-[1.01] hover:shadow-sm hover:shadow-brand-300 sm:grid-cols-3 md:grid-cols-4'
+    className={`text-14-400 grid h-8 grid-cols-2 items-center rounded-[6px] px-[16px] capitalize sm:grid-cols-3 md:grid-cols-4 ${
+      !view ? 'cursor-pointer hover:scale-[1.01] hover:shadow-sm hover:shadow-brand-300' : 'cursor-not-allowed select-none'
+    }`.trim()}
   >
-    <p className='cursor-pointer truncate pl-[16px]'>{contact.full_name ?? 'full name'}</p>
-    <p className='hidden cursor-pointer truncate sm:block'>
-      {contact.phone.replace(...phoneReplaceRegex) ?? '(**) *****-****'}
-    </p>
-    <p className='cursor-pointer truncate pr-[16px] lowercase md:pr-0'>{contact.email ?? 'mail@mail.com'}</p>
-    <p className='hidden cursor-pointer truncate pr-[16px] md:block'>
+    <p className='truncate pl-[16px] pr-[8px]'>{contact.full_name ?? 'full name'}</p>
+    <p className='hidden truncate pr-[8px] sm:block'>{contact.phone.replace(...phoneReplaceRegex) ?? '(**) *****-****'}</p>
+    <p className='truncate pr-[16px] lowercase md:pr-[8px]'>{contact.email ?? 'mail@mail.com'}</p>
+    <p className='hidden truncate pr-[16px] md:block'>
       {new Date(contact.created_at).toLocaleDateString('pt-BR') ?? '01/01/1999'}
     </p>
   </li>
--- frontend/src/components/ContactsList/index.tsx
+++ frontend/src/components/ContactsList/index.tsx
@@ -3,9 +3,11 @@ import { BsPersonPlus } from 'react-icons/bs';
 import { useContact } from 'src/hooks/useContact';
 import { ContactCard } from './Contact';
 
-interface Props extends HTMLAttributes<HTMLUListElement> {}
+interface Props extends HTMLAttributes<HTMLUListElement> {
+  view?: boolean;
+}
 
-export const ContactList = (props: Props) => {
+export const ContactList = ({ view, ...props }: Props) => {
   const {
     data: { contactList },
     contactService: { read },
@@ -16,11 +18,19 @@ export const ContactList = (props: Props) => {
   }, [read]);
 
   return (
-    <div className='contact-list-container flex h-full w-full flex-col gap-[32px] rounded-[16px] bg-grey-100 p-[16px] shadow-lg shadow-brand-100'>
+    <div className='tw-authenticated-page-main-content contact-list-container flex h-full w-full flex-col'>
       <div className='flex flex-col gap-[12px] px-[16px]'>
-        <button className='contact-list-container__new-contact self-end rounded-[8px] border border-brand-300 p-[8px] text-center text-brand-300 hover:bg-brand-300 hover:text-grey-50 focus:border-brand-100 focus:bg-brand-100 focus:text-brand-300'>
+        <button
+          disabled={view}
+          className={`contact-list-container__new-contact ${
+            view
+              ? 'border-transparent text-transparent opacity-0'
+              : 'border-brand-300 text-brand-300 hover:bg-brand-300 hover:text-grey-50 focus:border-brand-100 focus:bg-brand-100 focus:text-brand-300'
+          } self-end rounded-[8px] border p-[8px] text-center`.trim()}
+        >
           <BsPersonPlus />
         </button>
+
         <div className='contact-list-container__header text-12-700 grid-flow- grid grid-cols-2 gap-x-[8px] border-b border-grey-700 px-[16px] capitalize sm:grid-cols-3 md:grid-cols-4'>
           <h4 className='truncate'>nome</h4>
           <h4 className='hidden truncate sm:block'>telefone</h4>
@@ -28,9 +38,10 @@ export const ContactList = (props: Props) => {
           <h4 className='hidden truncate md:block'>cadastrado em</h4>
         </div>
       </div>
+
       <ul {...props} className='flex flex-col gap-[16px] md:gap-[24px]'>
         {contactList.map((cont) => (
-          <ContactCard contact={cont} key={cont.id} />
+          <ContactCard contact={cont} key={cont.id} view={view} />
         ))}
       </ul>
     </div>
