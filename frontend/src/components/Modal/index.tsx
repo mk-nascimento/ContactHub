8185f43d3a89b6d87d86c1d8182e51ddee25ae1f
Maksuel Nascimento <114680477+mk-nascimento@users.noreply.github.com>
2023-11-08 16:47:31 -0300

    refactor: Modal component changed to use <dialog/>

--- frontend/src/components/Modal/index.tsx
+++ frontend/src/components/Modal/index.tsx
@@ -1,47 +1,52 @@
-import { HTMLAttributes, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 import { IoClose } from 'react-icons/io5';
 
-interface IModalProps extends HTMLAttributes<HTMLDivElement> {
+interface IModalProps {
   children?: React.ReactNode;
   statePair: [boolean, React.Dispatch<React.SetStateAction<boolean>>];
 }
-export const Modal = ({ className, statePair, children, ...rest }: IModalProps) => {
+export const Modal = ({ children, statePair }: IModalProps) => {
   const [open, setOpen] = statePair;
-  const ref = useRef<HTMLDivElement>(null);
+  const dialog = useRef<HTMLDialogElement>(null);
+  const div = useRef<HTMLDivElement>(null);
 
-  const closeModal = () => setOpen(false);
+  const closeModal = () => {
+    setOpen(false);
+    dialog.current?.close();
+  };
 
   useEffect(() => {
     const handleOutClick = (event: MouseEvent) => {
-      if (!ref.current || !event.target) return;
-      if (!(event.target instanceof Node) || !ref.current.contains(event.target)) closeModal();
+      if (!div.current || !event.target) return;
+      if (!(event.target instanceof Node) || !div.current.contains(event.target)) closeModal();
     };
-    window.addEventListener('mousedown', handleOutClick);
+    if (dialog) window.addEventListener('mousedown', handleOutClick);
+
     return () => window.removeEventListener('mousedown', handleOutClick);
   });
+
+  useEffect(() => {
+    if (open && dialog.current) dialog.current.showModal();
+  }, [open]);
 
   return createPortal(
     <>
       {open && (
-        <div className='fixed inset-0 z-50 flex transform items-center justify-center bg-grey-100 bg-opacity-75 transition-opacity ease-in-out'>
-          <div
-            className={`${className} relative h-fit w-fit bg-grey-100 p-0 shadow-lg shadow-brand-100`.trim()}
-            {...rest}
-            ref={ref}
-            role='dialog'
-            aria-modal='true'
-            aria-labelledby='modal-title'
-          >
+        <dialog
+          ref={dialog}
+          className='w-11/12 max-w-sm rounded-[8px] border-none outline-none backdrop:bg-brand-neutral backdrop:bg-opacity-75 lg:max-w-md'
+        >
+          <div ref={div} className='relative w-full rounded-[8px] bg-grey-100 p-[32px] shadow-lg shadow-brand-100'>
             <button
               onClick={closeModal}
-              className='absolute right-4 top-4 animate-pulse text-xl text-grey-50 transition-colors duration-500 hover:text-brand-100'
+              className='absolute right-4 top-4 animate-pulse text-xl text-brand-100 transition-colors duration-500 hover:animate-none hover:text-brand-300'
             >
               <IoClose />
-              {children}
             </button>
+            {children}
           </div>
-        </div>
+        </dialog>
       )}
     </>,
     document.body,
