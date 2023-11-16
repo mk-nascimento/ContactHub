629a8e775affcd7a31a7cf6982e08bf702410b15
Maksuel Nascimento <114680477+mk-nascimento@users.noreply.github.com>
2023-11-16 17:17:54 -0300

    feat: enable CORS for prod consumption
      - Configured CORS settings to allow communication with the frontend application.
      - This enables secure cross-origin resource sharing.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 
-import { ValidationPipe } from '@nestjs/common';
+import { HttpStatus, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -8,6 +8,7 @@ async function bootstrap() {
   const PORT: number = +process.env.APP_PORT || 3000;
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({ origin: [process.env.CLIENT_URL, `${process.env.CLIENT_URL}/`], optionsSuccessStatus: HttpStatus.OK });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }), new ValidationPipe({ transform: true, transformOptions: { groups: ['transform'] } }));
 
   await app.listen(PORT);
